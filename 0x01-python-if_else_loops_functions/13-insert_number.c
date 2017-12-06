70aed12b1e9ad32266606b24df56d67f7dab587b
Raid55 <boutboulnicholas@gmail.com>
2017-12-06 01:24:56 +0000

    just out of the blue thought of some edge cases and fixed them

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,7 +9,7 @@
 listint_t *insert_node(listint_t **head, int number)
 {
 	int i = 0;
-	listint_t *tmp = NULL;
+	listint_t *tmp;
 	listint_t *new;
 
 	new = malloc(sizeof(listint_t));
@@ -19,7 +19,7 @@ listint_t *insert_node(listint_t **head, int number)
 	tmp = *head;
 	while (tmp != NULL)
 	{
-		if (tmp->n <= number && tmp->next->n >= number)
+		if (tmp->n <= number && (tmp->next == NULL || tmp->next->n >= number))
 			break;
 		tmp = tmp->next;
 		i++;
@@ -27,13 +27,11 @@ listint_t *insert_node(listint_t **head, int number)
 	tmp = *head;
 	while (i != 0)
 		tmp = tmp->next, i--;
-
+	
+	new->n = number, new->next = tmp ? tmp->next : NULL;
 	if (*head == NULL)
 		*head = new;
 	else
-	{
-		new->n = number, new->next = tmp->next;
 		tmp->next = new;
-	}
 	return (*head);
 }
