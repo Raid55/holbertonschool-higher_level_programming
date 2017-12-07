270de281c3ddeff544be94e8d9e107eff9c1dfe3
Raid55 <boutboulnicholas@gmail.com>
2017-12-07 10:31:25 +0000

    all peped up!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 from sys import argv
-print("{} arguments{}".format(len(argv) - 1, ":" if len(argv) > 1 else "."))
-i = 0
-for x in argv:
-    if i == 0:
+if __name__ == "__main__":
+    print("{} arguments{}".format(len(argv)-1, ":" if len(argv) > 1 else "."))
+    i = 1
+    for x in argv[1:]:
+        print("{}: {}".format(i, x))
         i += 1
-        continue
-    print("{}: {}".format(i, x))
-    i += 1
