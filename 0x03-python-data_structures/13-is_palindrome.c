1ef86a3edc98bfd7515f7c56aa26fa69870696a4
Raid55 <boutboulnicholas@gmail.com>
2017-12-13 03:25:15 +0000

    c thing

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ * is_palindrome - checks if linked list is palindrone
+ * @head: head of list
+ * Return: 1 or 0
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *start;
+	int *holder;
+	int i = 0, len = 0;
+
+	start = *head;
+
+	while (start != NULL)
+		len++, start = start->next;
+
+	holder = malloc(sizeof(int) * (len));
+	if (holder == NULL)
+		return (0);
+
+	i = 0, start = *head;
+	while (start != NULL)
+		holder[i++] = start->n, start = start->next;
+
+	/* i = 0; */
+	/* while (holder[i]) */
+		/* printf("%d", holder[i++]->n); */
+	i = 0, len--;
+	while (holder[i] == holder[len])
+	{
+		if (i == len || i > len)
+		{
+			free(holder);
+			return (1);
+		}
+		i++, len--;
+	}
+	free(holder);
+	return (0);
+}
