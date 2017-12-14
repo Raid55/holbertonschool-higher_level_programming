90c191d99591b3c46581f43a27b9075d5b2f1559
Raid55 <boutboulnicholas@gmail.com>
2017-12-14 22:31:14 +0000

    fixed 2 and wrote 4

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list is None:
-        num = 0
-    else:
-        num = list(set(my_list))[0]
-    for i in list(set(my_list))[1:]:
+    num = 0
+    for i in list(set(my_list)):
         num += i
     return num
