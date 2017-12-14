3d68b61c1fb6ed4911e9204ac182284fca4d0547
Raid55 <boutboulnicholas@gmail.com>
2017-12-14 22:43:11 +0000

    cool one liner with comprehenstion

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(my_dict):
-    new_dict = {}
-    for key, val in my_dict.items():
-        new_dict[key] = val * 2
-    return new_dict
+    return {key: val * 2 for key, val in my_dict.items()}
