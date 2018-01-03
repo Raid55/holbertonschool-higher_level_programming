2d6fcc30f13bba241ad25f0464c071cfab17734e
Raid55 <boutboulnicholas@gmail.com>
2018-01-03 07:56:03 +0000

    added count

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,10 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     num = 0;
     for i in range(0, x):
-        num = i
         try:
             print("{}".format(my_list[i]), end="")
         except:
             break;
+        num += 1
     print()
     return num
