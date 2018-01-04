1202b80733222983caaf0990931e5ac522cccb46
Raid55 <boutboulnicholas@gmail.com>
2018-01-04 03:09:12 +0000

    number one test

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+""" Just a Square class
+"""
+class Square:
+    """ a square class
+    """
+    def __init__(self, size=None):
+        self.__size = size
+    pass
