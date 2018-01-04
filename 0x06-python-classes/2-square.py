6b2468f0bad42889257ba8b181c8b219d8cb6a13
Raid55 <boutboulnicholas@gmail.com>
2018-01-04 03:21:06 +0000

    testing ex 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+""" Just a Square class
+"""
+class Square:
+    """ a square class
+    """
+    def __init__(self, size=0):
+        if isinstance(size, int) is not True:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    pass
