0cd7a6830096aa1abc0b528139ab8921566752e0
Nicholas Boutboul <boutboulnicholas@gmail.com>
2018-01-17 17:34:31 -0800

    removed line counter

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def write_file(filename="", text=""):
-    finNum = 0
     with open("my_first_file.txt", "w") as file:
-        for l in text:
-            finNum += 1
-            file.write(l)
-    return finNum
+        file.write(text)
