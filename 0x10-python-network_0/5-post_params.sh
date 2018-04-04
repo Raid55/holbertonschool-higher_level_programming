97ba83f4d372f09e7dff0f383c2334c6183bffa2
Raid55 <boutboulnicholas@gmail.com>
2018-04-04 00:33:51 +0000

    added d

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends request with params
-curl -sX POST "$1?email=hr@holbertonschool.com&subject=I+will+always+be+here+for+PLD"
+curl -sdX POST "$1?email=hr@holbertonschool.com&subject=I+will+always+be+here+for+PLD"
