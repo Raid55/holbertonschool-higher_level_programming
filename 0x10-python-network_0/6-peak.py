ba5468ba64adfe678b170cfe135d930ceba4d5f2
Raid55 <boutboulnicholas@gmail.com>
2018-04-04 00:09:40 +0000

    small chjange

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -14,3 +14,4 @@ def find_peak(list_of_integers):
         if (list_of_integers[i] > list_of_integers[i + 1] and
                 list_of_integers[i] > list_of_integers[i - 1]):
             return list_of_integers[i]
+    return list_of_integers[0]
