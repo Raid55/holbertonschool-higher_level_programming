38d63f6ea8cd2e60ff5d9419c0486c28ce9d0703
Raid55 <boutboulnicholas@gmail.com>
2018-04-09 23:33:24 +0000

    typo

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,5 +8,5 @@ from sys import argv
 
 if __name__ == "__main__":
     r = requests.get('https://api.github.com/users/' + argv[1],
-                    auth=HTTPBasicAuth(argv[1], argv[2]))
+                     auth=HTTPBasicAuth(argv[1], argv[2]))
     print(r.json().get('id'))
