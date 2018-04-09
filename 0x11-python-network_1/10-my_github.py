9f6671c1a3a22678da0e33665f1204cda4ae6b3e
Raid55 <boutboulnicholas@gmail.com>
2018-04-09 23:29:49 +0000

    github auth

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-""" lol """
+""" pings github """
+
+import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
 
 if __name__ == "__main__":
-    
+    r - request.get('https://api.github.com/users/' + argv[1],
+                    auth=HTTPBasicAuth(argv[1], argv[2]))
+    print(r.json().get('id'))
