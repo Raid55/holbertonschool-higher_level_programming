ef17e96d9a6d97ac1c50e78fc7aa8e69f65d2e43
Raid55 <boutboulnicholas@gmail.com>
2018-05-09 01:50:54 +0000

    small fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,5 +2,5 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', (err, contents) => {
-  if (contents) { console.log(contents); } else { console.log(err); }
+  if (err) { console.log(err); } else { console.log(contents); }
 });
